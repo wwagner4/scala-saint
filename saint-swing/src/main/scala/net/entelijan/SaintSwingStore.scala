ebdde8eed1005125372670fea29b22e36c5973b8
wwagner4 <wwagner4@gmail.com>
2016-03-04 14:08:08 +0100

    refactoring recorder

--- saint-swing/src/main/scala/net/entelijan/SaintSwingStore.scala
+++ saint-swing/src/main/scala/net/entelijan/SaintSwingStore.scala
@@ -9,16 +9,19 @@ import doctus.core.DoctusDraggable
 import doctus.core.DoctusScheduler
 import doctus.core.framework.DefaultDraggableController
 import java.io.File
+import akka.stream.scaladsl.Sink
+import scala.util.Success
+import scala.util.Failure
 
 object SaintSwingStore extends App with SaintSwing {
 
   val workdir = FileUtil.dir(List("saint", "data"))
-  val editMode = EM_Existing("1456934937699")
+  val editMode = EM_Existing("1456210674550")
   //val editMode = EM_New
 
   println("workdir: " + workdir)
   println("editMode: " + editMode)
-  
+
   run
 
   def runController(
@@ -38,13 +41,22 @@ case class RecorderReloaderStore(sched: DoctusScheduler, store: ImageStore, mat:
 
   def reload(id: String, consumer: RecordableConsumer): Future[Unit] = {
     store.recordableOut(id)
-      .runForeach({ rec => consumer.consume(rec) })(mat)
+      .runForeach(rec => consumer.consume(rec))(mat)
   }
 
   def recordTransport(transp: SaintTransport): Unit = {
-    Source.single(transp)
+    import scala.concurrent.ExecutionContext.Implicits.global
+    
+    val storeSink: Sink[Seq[Recordable], Future[_]] = store.recordableIn(transp.id)
+
+    val result = Source.single(transp)
       .map { t => t.recordables }
-      .runWith(store.recordableIn(transp.id))(mat)
+      .runWith(storeSink)(mat)
+
+    result onComplete {
+      case Success(_) => // Nothing to do
+      case Failure(t) => println("Error during recording: " + t.getMessage)
+    }
   }
 
 }
