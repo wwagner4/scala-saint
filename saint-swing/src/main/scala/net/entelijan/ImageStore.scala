bb893c6f6a3617598c5f3cf6a83f92a794155be8
wwagner4 <wwagner4@gmail.com>
2016-03-06 18:31:51 +0100

    refactoring store

--- saint-swing/src/main/scala/net/entelijan/ImageStore.scala
+++ saint-swing/src/main/scala/net/entelijan/ImageStore.scala
@@ -1,16 +1,15 @@
 package net.entelijan
 
 import java.io.File
-
 import scala.concurrent.ExecutionContext.Implicits
 import scala.concurrent.Future
-
 import akka.stream.scaladsl.FileIO
 import akka.stream.scaladsl.Flow
 import akka.stream.scaladsl.Keep
 import akka.stream.scaladsl.Sink
 import akka.stream.scaladsl.Source
 import akka.util.ByteString
+import akka.stream.io.Framing
 
 trait ImageStore {
 
@@ -22,7 +21,7 @@ trait ImageStore {
 class ImageStoreFilesys(_dir: File) extends ImageStore with ImageStoreBase {
 
   val dir = _dir
-  
+
   println("ImageStoreFilesys dir: " + dir)
 
   import scala.concurrent.ExecutionContext.Implicits.global
@@ -31,22 +30,21 @@ class ImageStoreFilesys(_dir: File) extends ImageStore with ImageStoreBase {
   require(dir.exists())
   require(dir.isDirectory())
 
-  
   def recordableOut(id: String): Source[Recordable, _] = {
     val file: File = getTxtFile(id).getOrElse(throw new IllegalStateException("no data found for id " + id))
 
-    val lines: Source[String, Future[_]] = FileLinesSource(file)
-
-    lines.map { line: String => upickle.default.read[Recordable](line) }
+    FileIO.fromFile(file)
+      .via(Framing.delimiter(ByteString("\n"), 100000))
+      .map { bs: ByteString => bs.utf8String }
+      .map { line: String => upickle.default.read[Recordable](line) }
   }
 
   def recordableIn(id: String): Sink[Seq[Recordable], Future[Long]] = {
     val file = txtFile(id)
-    
-    val flow = Flow[Seq[Recordable]].map { write }
-    val sink = FileIO.toFile(file, append = true)
 
-    flow.toMat(sink)(Keep.right)
+    Flow[Seq[Recordable]]
+      .map { reqSeq: Seq[Recordable] => write(reqSeq) }
+      .toMat(FileIO.toFile(file, append = true))(Keep.right)
   }
 
   private def write(recs: Seq[Recordable]): ByteString = {
--- saint-swing/src/test/scala/net/entelijan/ReadFileTryout.scala
+++ saint-swing/src/test/scala/net/entelijan/ReadFileTryout.scala
@@ -3,18 +3,22 @@ package net.entelijan
 import akka.actor.ActorSystem
 import akka.stream._
 import akka.stream.scaladsl._
-
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import akka.stream.io.Framing
+import akka.util.ByteString
 
 object ReadFileTryout extends App {
 
   implicit val sys = ActorSystem()
-  
+
   try {
     implicit val mat = ActorMaterializer()
     val path = "saint-swing/src/test/resources/saint_1443162064186.txt"
-    val src: Source[String, _] = FileLinesSource(new java.io.File(path))
+    val file = new java.io.File(path)
+    val src: Source[String, _] = FileIO.fromFile(file)
+      .via(Framing.delimiter(ByteString("\n"), 10000))
+      .map { bs: ByteString => bs.utf8String }
 
     val result = src.runForeach(s => println("line0:'%s'" format s))
     Thread.sleep(10)
@@ -31,6 +35,5 @@ object ReadFileTryout extends App {
   } finally {
     sys.shutdown()
   }
-
 
 }
