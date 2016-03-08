f40f0170f36ce9e758aa64614a476ec184cd1907
wwagner4 <wwagner4@gmail.com>
2016-03-08 07:41:19 +0100

    average througput can be measured

--- saint-swing/src/test/scala/net/entelijan/GraphStageTryout.scala
+++ saint-swing/src/test/scala/net/entelijan/GraphStageTryout.scala
@@ -16,40 +16,54 @@ import akka.stream.stage.InHandler
 import akka.stream.stage.OutHandler
 
 object GraphStageTryout extends App {
-  
+
   implicit val sys = ActorSystem()
   implicit val mat = ActorMaterializer()
-  
+
   println("hello")
-  
-  val src = Source.fromIterator { () => (1 to 10).toList.iterator }
-  
+
+  val src = Source.fromIterator { () => (1 to 100).toList.iterator }
+
   val map = new Monitor[Int]
-  
+
   val r = src
     .via(map)
-    .map {x => Thread.sleep(10); x}
+    .map { x => Thread.sleep(40); x }
     .runForeach { x => }
-  
+
   Await.ready(r, 5.seconds)
   sys.shutdown()
 }
 
 class Monitor[A] extends GraphStage[FlowShape[A, A]] {
- 
-  val in = Inlet[A]("Map.in")
-  val out = Outlet[A]("Map.out")
- 
+
+  val in = Inlet[A]("Monitor.in")
+  val out = Outlet[A]("Monitor.out")
+
   override val shape = FlowShape.of(in, out)
-  
+
   var time = Option.empty[Long]
- 
+  var startTime = System.currentTimeMillis()
+  var cnt = 0
+  var sum = 0L
+
   override def createLogic(attr: Attributes): GraphStageLogic =
     new GraphStageLogic(shape) {
       setHandler(in, new InHandler {
         override def onPush(): Unit = {
           val now = System.currentTimeMillis()
-          time.foreach { prev => println(now - prev) }
+          time.foreach { prev =>
+            val diff = now - prev
+            sum += diff
+            cnt += 1
+          }
+          if (now - startTime > 500) {
+            val avr = sum.toDouble / cnt
+            println("average throughput: %.3f" format avr)
+            startTime = now
+            sum = 0
+            cnt = 0
+          }
           time = Some(now)
           push(out, grab(in))
         }
