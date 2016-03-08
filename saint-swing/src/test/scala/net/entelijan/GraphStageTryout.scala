8c50e2ddf27e6699928223d68b03ba4f02f0b59c
wwagner4 <wwagner4@gmail.com>
2016-03-08 07:24:04 +0100

    showing some time diff OK

--- saint-swing/src/test/scala/net/entelijan/GraphStageTryout.scala
+++ saint-swing/src/test/scala/net/entelijan/GraphStageTryout.scala
@@ -1,10 +1,19 @@
 package net.entelijan
 
-import akka.stream.scaladsl.Source
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import scala.concurrent._
-import scala.concurrent.duration._
+import akka.stream.Attributes
+import akka.stream.FlowShape
+import akka.stream.Inlet
+import akka.stream.Outlet
+import akka.stream.scaladsl.Source
+import akka.stream.stage.GraphStage
+import akka.stream.stage.GraphStageLogic
+import akka.stream.stage.InHandler
+import akka.stream.stage.OutHandler
 
 object GraphStageTryout extends App {
   
@@ -13,11 +22,44 @@ object GraphStageTryout extends App {
   
   println("hello")
   
-  val iter = (1 to 100).toList
+  val src = Source.fromIterator { () => (1 to 10).toList.iterator }
   
-  val src = Source.fromIterator { () => (1 to 100).toList.iterator }
+  def f(in: Int): Int = in * 2
   
-  val r = src.runForeach { x => println(x) }
+  val map = new Map[Int, Int](f)
+  
+  val r = src
+    .via(map)
+    .map {x => Thread.sleep(10); x}
+    .runForeach { x => }
   
   Await.ready(r, 5.seconds)
+  sys.shutdown()
+}
+
+class Map[A, B](f: A => B) extends GraphStage[FlowShape[A, B]] {
+ 
+  val in = Inlet[A]("Map.in")
+  val out = Outlet[B]("Map.out")
+ 
+  override val shape = FlowShape.of(in, out)
+  
+  var time = Option.empty[Long]
+ 
+  override def createLogic(attr: Attributes): GraphStageLogic =
+    new GraphStageLogic(shape) {
+      setHandler(in, new InHandler {
+        override def onPush(): Unit = {
+          val now = System.currentTimeMillis()
+          time.foreach { prev => println(now - prev) }
+          time = Some(now)
+          push(out, f(grab(in)))
+        }
+      })
+      setHandler(out, new OutHandler {
+        override def onPull(): Unit = {
+          pull(in)
+        }
+      })
+    }
 }
